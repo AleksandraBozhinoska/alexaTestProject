32a44848094965bf84bc730f00670a6105a2b205
Aleksandra Bozhinoska <Aleksandra.Bozhinoska@netcetera.com>
2018-07-25 13:56:59 +0200

    Initial project added

---
+++ helloAlexa/src/main/java/mk/ukim/finki/helloAlexa/HelloAlexaApplication.java
@@ -0,0 +1,12 @@
+package mk.ukim.finki.helloAlexa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloAlexaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloAlexaApplication.class, args);
+	}
+}
